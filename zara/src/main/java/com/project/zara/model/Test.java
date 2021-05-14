8225c258b7329d2dc55d44aba913ecf9f872af98
park517 <park51777@naver.com>
2021-05-14 11:09:16 +0900

    기본 설정

---
+++ zara/src/main/java/com/project/zara/model/Test.java
@@ -0,0 +1,13 @@
+package com.project.zara.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Test {
+	private String name;
+	private Integer age;
+}
