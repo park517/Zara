0e4153751b97d5eab5cec61dd660126168dfcd6e
park517 <park51777@naver.com>
2021-07-26 21:03:34 +0900

    브런치 생성

--- zara/java/com/project/zara/controller/ProductController.java
+++ zara/java/com/project/zara/controller/ProductController.java
@@ -28,11 +28,9 @@ import com.project.zara.service.ReplyService;
 import com.project.zara.util.FileUtil;
 import com.project.zara.util.PagingUtil;
 
-import jdk.internal.org.jline.utils.Log;
 import lombok.extern.log4j.Log4j;
 
 @Controller
-@Log4j
 // 중고 자전거 판매 
 @RequestMapping("/product")
 public class ProductController {
